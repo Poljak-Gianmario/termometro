0f09d51a80d1375a42ac054a77672fda64f35b2f
poljak <gianmariopoljak00@protonmail.ch>
2023-05-28 16:07:49 +0200

    Added my project

--- App.js
+++ App.js
@@ -1,11 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
+import React, { useState } from "react";
+import { Dimensions } from "react-native";
+import { AppBar } from "@react-native-material/core";
+import { Picker } from "@react-native-picker/picker";
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
+
 
 export default function App() {
+  
+
+  const [selectedValue, setSelectedValue] = useState("Giorno");
+
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     
+      <View >
+      <AppBar title="Resoconto casa" />
+        <Picker
+          selectedValue={selectedValue}
+          style={{ height: 50, width: 150 }}
+          onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+        >
+          <Picker.Item label="Giorno" value="Giorno" />
+          <Picker.Item label="Settimana" value="Settimana" />
+        </Picker>
+        
+      <Text>Temperatura oggi</Text>
+          <LineChart
+            data={{
+              labels: ["January", "February", "March", "April", "May", "June"],
+              datasets: [
+                {
+                  data: [
+                    Math.random() * 100,
+                    Math.random() * 100,
+                    Math.random() * 100,
+                    Math.random() * 100,
+                    Math.random() * 100,
+                    Math.random() * 100
+                  ]
+                }
+              ]
+            }}
+            width={Dimensions.get("window").width} // from react-native
+            height={220}
+            yAxisLabel="$"
+            yAxisSuffix="k"
+            yAxisInterval={1} // optional, defaults to 1
+            chartConfig={{
+              backgroundColor: "#e26a00",
+              backgroundGradientFrom: "#fb8c00",
+              backgroundGradientTo: "#ffa726",
+              decimalPlaces: 2, // optional, defaults to 2dp
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              style: {
+                borderRadius: 16
+              },
+              propsForDots: {
+                r: "6",
+                strokeWidth: "2",
+                stroke: "#ffa726"
+              }
+            }}
+            bezier
+            style={{
+              marginVertical: 8,
+              borderRadius: 16
+            }}
+          />
+        </View>
     </View>
   );
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+# PROGETTO TPI APP MOBILE TEMPERATURA
+
+Librerie utilizzate:
+https://www.npmjs.com/package/react-native-chart-kit
+https://www.react-native-material.com/docs/components/app-bar
