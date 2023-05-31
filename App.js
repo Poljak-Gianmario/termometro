518412d741d9691d9b092f0bba9ec8cd8d2e51d2
poljak <gianmariopoljak00@protonmail.ch>
2023-05-31 22:31:50 +0200

    update

--- App.js
+++ App.js
@@ -1,84 +1,28 @@
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, ScrollView} from 'react-native';
 import React, { useState } from "react";
-import { Dimensions } from "react-native";
 import { AppBar } from "@react-native-material/core";
-import { Picker } from "@react-native-picker/picker";
-import {
-  LineChart,
-  BarChart,
-  PieChart,
-  ProgressChart,
-  ContributionGraph,
-  StackedBarChart
-} from "react-native-chart-kit";
-
+import Temp_stat from './Components/temp_stat';
+import Umid_stat from './Components/umid_stat';
+import Gas_stat from './Components/gas_stat';
 
 export default function App() {
   
 
-  const [selectedValue, setSelectedValue] = useState("Giorno");
 
   return (
     
-    <View style={styles.container}>
-     
-      <View >
+    <ScrollView 
+    style={styles.scrollView} 
+    contentContainerStyle={styles.contentContainer}
+  >     
+
       <AppBar title="Resoconto casa" />
-        <Picker
-          selectedValue={selectedValue}
-          style={{ height: 50, width: 150 }}
-          onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-        >
-          <Picker.Item label="Giorno" value="Giorno" />
-          <Picker.Item label="Settimana" value="Settimana" />
-        </Picker>
-        
-      <Text>Temperatura oggi</Text>
-          <LineChart
-            data={{
-              labels: ["January", "February", "March", "April", "May", "June"],
-              datasets: [
-                {
-                  data: [
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100,
-                    Math.random() * 100
-                  ]
-                }
-              ]
-            }}
-            width={Dimensions.get("window").width} // from react-native
-            height={220}
-            yAxisLabel="$"
-            yAxisSuffix="k"
-            yAxisInterval={1} // optional, defaults to 1
-            chartConfig={{
-              backgroundColor: "#e26a00",
-              backgroundGradientFrom: "#fb8c00",
-              backgroundGradientTo: "#ffa726",
-              decimalPlaces: 2, // optional, defaults to 2dp
-              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-              style: {
-                borderRadius: 16
-              },
-              propsForDots: {
-                r: "6",
-                strokeWidth: "2",
-                stroke: "#ffa726"
-              }
-            }}
-            bezier
-            style={{
-              marginVertical: 8,
-              borderRadius: 16
-            }}
-          />
-        </View>
-    </View>
+      
+      <Temp_stat/>
+      <Umid_stat/>
+      <Gas_stat/>
+
+    </ScrollView>
   );
 }
 
---
+++ Components/umid_stat.js
@@ -0,0 +1,134 @@
+import {Text, View} from 'react-native';
+import React, { useState } from "react";
+import { Dimensions } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import {
+  LineChart,
+} from "react-native-chart-kit";
+
+export default function Umid_stat(){
+
+    const [selectedValue, setSelectedValue] = useState("Giorno");
+
+    const label_giorno = [
+    "00-04",
+    "04-08",
+    "08-12",
+    "12-16",
+    "16-20",
+    "20-24"
+    ];
+
+    const label_settimana = [
+    "Lun",
+    "Mar",
+    "Mer",
+    "Gio",
+    "Ven",
+    "Sab",
+    "Dom"
+    ];
+
+    const label_mese = [
+    "Gen",
+    "Feb",
+    "Mar",
+    "Apr",
+    "Mag",
+    "Giu",
+    "Lug",
+    "Ago",
+    "Set",
+    "Ott",
+    "Nov",
+    "Dic"
+    ];
+
+    var scelta = [];
+
+  switch(selectedValue){
+
+        case "giorno":
+            scelta = label_giorno.slice();
+            break;
+        case "settimana":
+            scelta = label_settimana.slice();
+            break;
+        case "mese":
+            scelta = label_mese.slice();
+            break;
+        default:
+            scelta = label_giorno.slice(); 
+            break;
+
+    }
+  
+    return(
+
+        <View>
+          <Picker
+            selectedValue={selectedValue}
+            style={{ height: 50, width: 160 }}
+            onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+          >
+            <Picker.Item label="Giorno" value="giorno" />
+            <Picker.Item label="Settimana" value="settimana" />
+            <Picker.Item label="Mese" value="mese" />
+          </Picker>
+
+  
+          
+        <Text>Umidità {selectedValue}:</Text>
+
+            <LineChart
+              data={{
+ 
+                labels: scelta,
+                datasets: [
+                  {
+                    data: [
+                     20,
+                     30,
+                     40,
+                     25,
+                     25,
+                     20,
+                     30,
+                     40,
+                     25,
+                     25,
+                     0
+                    ]
+                  }
+                ]
+              }}
+              width={Dimensions.get("window").width} // from react-native
+              height={220}
+              yAxisLabel="%"
+              yAxisInterval={1} // optional, defaults to 1
+              chartConfig={{
+                backgroundColor: "#e26a00",
+                backgroundGradientFrom: "#94dce9",
+                backgroundGradientTo: "#94dce9",
+                decimalPlaces: 0, // optional, defaults to 2dp
+                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                style: {
+                  borderRadius: 16
+                },
+                propsForDots: {
+                  r: "6",
+                  strokeWidth: "2",
+                  stroke: "#ffa726"
+                }
+              }}
+              bezier
+              style={{
+                marginVertical: 8,
+                borderRadius: 16
+              }}
+            />
+        </View>
+
+    )
+}
